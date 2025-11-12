b30d6ecd7a4bb11697496b96f82afedb30f3edb8
Philip Silva <philip.silva@protonmail.com>
2025-11-12 13:17:24 +0100

    add github action

--- dotnet-cc/hostfunc_patch/hostfunc_patch.sh
+++ dotnet-cc/hostfunc_patch/hostfunc_patch.sh
@@ -4,10 +4,13 @@ set -xeu -o pipefail
 
 SOURCE_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-/wabt/bin/wasm2wat mod/unbundled-module0-unpatched.wasm > mod/unbundled-module0-patched.wat
+ln -sf mod/unbundled-module1.wasm mod/wasi_snapshot_preview1.wasm
+cp mod/unbundled-module0.wasm mod/unbundled-module0-unpatched.wasm
+
+wasm2wat mod/unbundled-module0-unpatched.wasm > mod/unbundled-module0-patched.wat
 
 go run $SOURCE_DIR/hostfunc_patch.go -in mod/unbundled-module0-patched.wat -out /tmp/unbundled-module0-patched-2.wat -funs hostFuncPrintk,hostFuncLenInputBuf,hostFuncReadValue,hostFuncShutdown
 cp /tmp/unbundled-module0-patched-2.wat mod/unbundled-module0-patched.wat
 
-/wabt/bin/wat2wasm mod/unbundled-module0-patched.wat -o mod/unbundled-module0-patched.wasm
+wat2wasm mod/unbundled-module0-patched.wat -o mod/unbundled-module0-patched.wasm
 cp mod/unbundled-module0-patched.wasm mod/unbundled-module0.wasm
